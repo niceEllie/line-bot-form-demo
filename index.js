d689dfffa676bfb1b90fee71c59efa6d0e162864
niceEllie <catfly46@gmail.com>
2025-06-07 14:30:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const bodyParser = require("body-parser");
 const line = require("./line");
 
 const app = express();
-app.use(bodyParser.json());
 
-app.post("/webhook", line.handler);
+app.use(bodyParser.json({
+  verify: (req, res, buf) => {
+    req.rawBody = buf;
+  }
+}));
+
+app.post("/webhook", (req, res, next) => {
+  req.body = JSON.parse(req.rawBody.toString());
+  next();
+}, line.handler);
+
 app.get("/", (req, res) => res.send("LINE Bot Demo Running"));
 
 const port = process.env.PORT || 3000;
